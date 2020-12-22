d23909efc8b1a2399c038f71a5bb3d0a59937fbf
Olesya Miller <olesyam.miller@gmail.com>
2020-12-21 20:51:49 -0400

    fix spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_many :purchases 
     has_many :books, through: :purchases
-    
     has_secure_password
     validates :username, :email, :password_digest, presence: true, uniqueness: true
     validates :credit, presence: true
